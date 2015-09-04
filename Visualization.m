1b7b7a7e6c8323eaded8ccce2fa39a1b7b2ba33e
unknown <hqkhor95@gmail.com>
2015-09-04 17:48:02 +0800

    Object Position detected on video, but weird yellow background appears

--- Visualization.m
+++ Visualization.m
@@ -3,23 +3,55 @@ function Visualization(dataDir,filename)
     dataFile = strcat(dataDir, '\', filename, ' - TackLabels_PHOW.dat');
     tracksFile = strcat(dataDir,'\', filename, ' - tracks.txt');
     
+    videoFileReader = vision.VideoFileReader(video);
     a = load(tracksFile);
+   
     
-    fid= fopen(tracksFile);
+    %fid= fopen(tracksFile);
     
-    b=dlmread(tracksFile,' ',[0 2 2 5]);
-    disp(b);
+    %==========================================================%
+    %  Extracting Frames and Object Position from Tracks File  %                                         
+    %==========================================================%
     
+
+    %b=dlmread(tracksFile,' ',[0 1 iterator 5]);
+    %disp(b);
     
+    videoInfo = info(videoFileReader);
+    videoPlayer = vision.VideoPlayer('Position',[200 200 videoInfo.VideoSize+30]);
     
+    while ~isDone(videoFileReader)
+        %Extract next frame
+        videoFrame = step(videoFileReader);
         
-    %end
+        %Extract Position of Bounding Box
+        
+        w = a(:,3);
+        x = a(:,4);
+        y = a(:,5);
+        z = a(:,6);
+        
+        position = [w x y z];  
+        
+        %Draw Out 
+        videoOut = insertObjectAnnotation(videoFrame,'rectangle',position,'Object');
+        
+        step(videoPlayer,videoOut);
+        
+       
+    end
+    
+    % Release resources
+     figure, imshow(videoOut);
+release(videoFileReader);
+release(videoPlayer);
+   
     %===========================================================%
     %                  Displaying Box on Video                  %
     %===========================================================%
-    videoFileReader = vision.VideoFileReader(video);
-    videoFrame= step(videoFileReader);
-    
-    videoOut = insertObjectAnnotation(videoFrame,'rectangle',rectangle,'Object');
-    figure, imshow(videoOut), title('LoViS');
+%     videoFileReader = vision.VideoFileReader(video);
+%     videoFrame= step(videoFileReader);
+%     
+%     videoOut = insertObjectAnnotation(videoFrame,'rectangle',rectangle,'Object');
+%     figure, imshow(videoOut), title('LoViS');
 end
