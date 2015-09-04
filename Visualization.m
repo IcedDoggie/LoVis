b378bd1031b49acc6fc257c78d6b9cf95f9890f8
unknown <hqkhor95@gmail.com>
2015-09-04 18:03:21 +0800

    Bounding Box appears, but it detects ghost?

--- Visualization.m
+++ Visualization.m
@@ -19,17 +19,17 @@ function Visualization(dataDir,filename)
     
     videoInfo = info(videoFileReader);
     videoPlayer = vision.VideoPlayer('Position',[200 200 videoInfo.VideoSize+30]);
-    
+    iterator = 1;
     while ~isDone(videoFileReader)
         %Extract next frame
         videoFrame = step(videoFileReader);
         
         %Extract Position of Bounding Box
         
-        w = a(:,3);
-        x = a(:,4);
-        y = a(:,5);
-        z = a(:,6);
+        w = a(iterator,3);
+        x = a(iterator,4);
+        y = a(iterator,5);
+        z = a(iterator,6);
         
         position = [w x y z];  
         
@@ -37,7 +37,7 @@ function Visualization(dataDir,filename)
         videoOut = insertObjectAnnotation(videoFrame,'rectangle',position,'Object');
         
         step(videoPlayer,videoOut);
-        
+        iterator = iterator + 1;
        
     end
     
