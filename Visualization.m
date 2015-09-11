96efb05e7daea266cfcf5b1f4587734124616299
unknown <hqkhor95@gmail.com>
2015-09-11 14:49:09 +0800

    target frames extracting enabled

--- Visualization.m
+++ Visualization.m
@@ -5,7 +5,8 @@ function Visualization(dataDir,filename)
     
     
     videoFileReader = vision.VideoFileReader(video);
-    a = load(tracksFile);
+    %VARIABLE: tracksList -> matrix extracted from tracks text file
+    tracksList = load(tracksFile);
    
     fid = fopen(dataFile);
     trackdata = textscan(fid, '%d%s', 'delimiter', ',');
@@ -24,41 +25,47 @@ function Visualization(dataDir,filename)
     
     videoInfo = info(videoFileReader);
     videoPlayer = vision.VideoPlayer('Position',[200 200 videoInfo.VideoSize+30]);
-%     iterator = 1;
+    %VARIABLE: iterator-> a helper variable in counting video frames
+    iterator = 1;
   
     while ~isDone(videoFileReader)
-        %Extract next frame
+        % Extract next frame
         videoFrame = step(videoFileReader);
         
         %Find the object in the specific Frame
+        %VARIABLE: boundObjects -> determine the existence of object in a
+        %specifc frame. Output is a matrix. of detected Frames.
+        boundObjects = tracksList(tracksList(:,2)==iterator,:);
+            
+
+        % Extract Position of Bounding Box
         
+        trackID = a(iterator,1);      
+        xPos = a(iterator,3);      
+        yPos = a(iterator,4);
+        width = a(iterator,5);
+        height = a(iterator,6);
         
-        %Extract Position of Bounding Box
+        position = [x y w h];  
         
-%         s = a(iterator,1);      % track id
-%         x = a(iterator,3);      % 
-%         y = a(iterator,4);
-%         w = a(iterator,5);
-%         h = a(iterator,6);
-%         
-%         position = [x y w h];  
+        % Determine what kind of object it is
         
-        %Determine what kind of object it is
-        objectType = dataFile(2:end,2);
-        objectID = dataFile(2:end,1);
-        if(s==objectID)
-             objectDisplayed = dataFile(objectID,objectType);
-        else
-            objectDisplayed = '-';
-        end
-        %Draw it Out in video frames
-        videoOut = insertObjectAnnotation(videoFrame,'rectangle',position,objectDisplayed);
+%         objectType = dataFile(2:end,2);
+%         objectID = dataFile(2:end,1);
+%         if(s==objectID)
+%              objectDisplayed = dataFile(objectID,objectType);
+%         else
+%             objectDisplayed = '-';
+%         end
+
+        % Draw it Out in video frames
+        videoOut = insertObjectAnnotation(videoFrame,'rectangle',position,'object');
     
         % Print frame number
         videoOut = insertText(videoOut,[3 3],iterator,'AnchorPoint','LeftTop');
         
         step(videoPlayer,videoOut);
-%         iterator = iterator + 1;
+        iterator = iterator + 1;
         pause(0.25);
     end
 
