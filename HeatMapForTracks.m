cb2c3e94f2f14ae57e762a9f8cef674f47162ac1
iceboy95 <hqkhor95@gmail.com>
2015-11-15 17:51:59 +0800

    A video resolution matrix initialized, with heatmap function enabled

--- HeatMapForTracks.m
+++ HeatMapForTracks.m
@@ -4,24 +4,28 @@ function HeatMapForTracks(dataDir,filename)
     
     posMatrix = tracksList(:,3:4);
     
-    % Getting the video Frame Size
+    % Getting the video Frame Size, row and columns 
     videoFile = strcat(dataDir, '\', filename, '.avi');
     videoFileReader = VideoReader(videoFile);
     video = readFrame(videoFileReader);
     vidWidth = videoFileReader.Width;
     vidHeight = videoFileReader.Height;
     
+    vidRow = zeros(vidWidth);
+    vidRow = vidRow(1,:);
+    
+    vidColumn = zeros(vidHeight);
+    vidColumn = vidColumn(:,1);
+    
     % Getting the total number of rows
     totalRow = size(posMatrix);
     totalRow = totalRow(1,1);
     totalColumn = 2;
     
-    % Convert Row and Columns to cell
-%     totalRowCell = cell(totalRow,1); 
-%     for i = 1:totalRow
-%         singleRow = posMatrix(i,1:2);
-%         totalRowCell(i,1) = {singleRow}; 
-%     end
+    % Initializing matrix for heatmapObj
+    heatmapMatrix = zeros(vidWidth,vidHeight);
+    
+    heatmapMatrix(1:vidWidth,1:vidHeight) = 6;
     
     % Get the max rows and columns from data
     maxRow = posMatrix(:,1);
@@ -29,7 +33,7 @@ function HeatMapForTracks(dataDir,filename)
     maxColumn = posMatrix(:,2);
     maxColumn = max(maxColumn);
     
-    heatmapObj = HeatMap(posMatrix,'Colormap',redbluecmap,'RowLabels',vidHeight,'ColumnLabels',vidWidth);
+    heatmapObj = HeatMap(heatmapMatrix,'Colormap',redgreencmap,'RowLabels',vidRow,'ColumnLabels',vidColumn);
     
 
 end
