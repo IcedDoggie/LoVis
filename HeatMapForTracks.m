04502d7c65b7ec75423daac63390c765fea295d9
iceboy95 <hqkhor95@gmail.com>
2015-11-18 23:23:26 +0800

    Heatmap enabled. Yet to increase size of footprints and smooth it out using gaussian

--- HeatMapForTracks.m
+++ HeatMapForTracks.m
@@ -3,6 +3,9 @@ function HeatMapForTracks(dataDir,filename)
     tracksList = load(tracksFile);
     
     posMatrix = tracksList(:,3:4);
+    maxCount = numel(posMatrix(:,1));
+    
+    imageWorld = 'ExpPicture.png';
     
     % Getting the video Frame Size, row and columns 
     videoFile = strcat(dataDir, '\', filename, '.avi');
@@ -16,24 +19,46 @@ function HeatMapForTracks(dataDir,filename)
     
     vidColumn = zeros(vidHeight);
     vidColumn = vidColumn(:,1);
-    
-    % Getting the total number of rows
-    totalRow = size(posMatrix);
-    totalRow = totalRow(1,1);
-    totalColumn = 2;
-    
+        
     % Initializing matrix for heatmapObj
     heatmapMatrix = zeros(vidWidth,vidHeight);
     
-    heatmapMatrix(1:vidWidth,1:vidHeight) = 6;
-    
+    dummyData = heatmapMatrix;
+
+
     % Get the max rows and columns from data
     maxRow = posMatrix(:,1);
     maxRow = max(maxRow);
     maxColumn = posMatrix(:,2);
     maxColumn = max(maxColumn);
     
-    heatmapObj = HeatMap(heatmapMatrix,'Colormap',redgreencmap,'RowLabels',vidRow,'ColumnLabels',vidColumn);
+    % Parse in the values from dataset to heatmapMatrix
+    for i=1 : maxCount
+        singleRowPosX = posMatrix(i,1);
+        singleRowPosY = posMatrix(i,2);   
+        heatmapMatrix(singleRowPosX,singleRowPosY) = heatmapMatrix(singleRowPosX,singleRowPosY) + 4;
+        
+        
+    end
+    
+%     for i=1 : 640
+%         dummyData(200,i) = 1 + i;
+%     end
+
+    %heatmapObj = HeatMap(heatmapMatrix,'ColorMap',redbluecmap,'RowLabels',vidRow,'ColumnLabels',vidColumn);
+    %heatmapObj2 = HeatMap(posMatrix,'ColorMap',redbluecmap);
+    %heatmapObj3 = HeatMap(heatmapMatrix);
+    
+%     colormap('jet');
+%       imageHeatData = imagesc(heatmapMatrix);
+%       colorbar;
+%       
+      imagesc(dummyData);
+      colorbar;
+      
+      
+     % imageOutputCombined = imfuse(imageHeatData,imageWorld);
+    
     
 
 end
